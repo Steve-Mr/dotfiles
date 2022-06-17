12dbfaa21bcf94499a861cd4d2dc504c48fcec8c
maary <dicedsteve@gmail.com>
2022-06-17 21:59:33 +0800

    add regolith and polybar config

---
+++ .config/polybar/scripts/rofication.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+rofication_print() {
+    status=$(/home/maary/.config/polybar/scripts/rofication-status)
+    if [ $status = '?' ]; then
+        printf $status
+    elif [ $status -gt 0 ]; then
+        printf " %s" "$status"
+    else
+        printf " 0"
+    fi
+    printf '\n'
+}
+
+rofication_show() {
+    ilia -p notifications
+}
+
+case "$1" in
+    --show)
+        rofication_show
+        ;;
+    *)
+        rofication_print
+        ;;
+esac
