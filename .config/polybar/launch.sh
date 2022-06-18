ce78cbf063ba94e2e501f70b833701265d839e55
maary <dicedsteve@gmail.com>
2022-06-18 09:39:17 +0800

    set tray position

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,14 +1,26 @@
-#!/bin/bash
-
-# 终端可能已经有在运行的实例
-killall -q polybar
-
-# 等待进程被终止
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# 运行Polybar，使用默认的配置文件路径 ~/.config/polybar/config
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-			MONITOR=$m polybar -q example &	
-	done
-
+#!/bin/bash                                                                        
+                                                                                   
+# Terminate already running bars                                                   
+killall -q polybar                                                                 
+                                                                                   
+# Launch new polybar(s)                                                            
+if type "xrandr"; then                                                             
+    IFS=$'\n'  # must set internal field separator to avoid dumb                   
+    for entry in $(xrandr --query | grep " connected"); do                         
+        mon=$(cut -d" " -f1 <<< "$entry")                                          
+        status=$(cut -d" " -f3 <<< "$entry")                                       
+                                                                                   
+        tray_pos=""                                                                
+        if [ "$status" == "primary" ]; then                                        
+            tray_pos="right"                                                       
+        fi                                                                         
+                                                                                   
+        MONITOR=$mon TRAY_POS=$tray_pos polybar -r example 2>&1 | tee -a /tmp/polyb\
+ar-monitor-"$mon".log & disown                                                     
+    done                                                                           
+    unset IFS  # avoid mega dumb by resetting the IFS                              
+else                                                                               
+    polybar -r example 2>&1 | tee -a /tmp/polybar.log & disown                      
+fi                                                                                 
+                                                                                   
 echo "Polybar launched..."
