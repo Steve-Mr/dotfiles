2c0028e5c6fe8de7a231618c0699a41ca75f74ef
maary <24504742+Steve-Mr@users.noreply.github.com>
2022-10-10 21:53:47 +0800

    change polybar theme

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,5 +1,7 @@
-#!/bin/bash                                                                        
-                                                                                   
+#!/bin/bash              
+
+DIR="$HOME/.config/polybar/polybar-themes/simple/material"                                                         
+
 # Terminate already running bars                                                   
 killall -q polybar                                                                 
                                                                                    
@@ -15,12 +17,14 @@ if type "xrandr"; then
             tray_pos="right"                                                       
         fi                                                                         
                                                                                    
-        MONITOR=$mon TRAY_POS=$tray_pos polybar -r example 2>&1 | tee -a /tmp/polyb\
-ar-monitor-"$mon".log & disown                                                     
+        # MONITOR=$mon TRAY_POS=$tray_pos polybar -r example 2>&1 | tee -a /tmp/polybar-monitor-"$mon".log & disown                                                     
+    MONITOR=$mon TRAY_POS=$tray_pos polybar -q main -c "$DIR"/config.ini 2>&1 | tee -a /tmp/polybar-monitor-"$mon".log & disown
     done                                                                           
     unset IFS  # avoid mega dumb by resetting the IFS                              
 else                                                                               
-    polybar -r example 2>&1 | tee -a /tmp/polybar.log & disown                      
+    # polybar -r example 2>&1 | tee -a /tmp/polybar.log & disown
+    polybar -q main -c "$DIR"/config.ini 2>&1 | tee -a /tmp/polybar.log & disown                      
+
 fi                                                                                 
                                                                                    
 echo "Polybar launched..."
