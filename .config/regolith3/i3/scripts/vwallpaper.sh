f5b4ea025a7712e7860e8837850eb3f5dd47b65b
Maary <24504742+Steve-Mr@users.noreply.github.com>
2024-04-24 15:39:32 +0800

    240424 update

--- .config/regolith3/i3/scripts/vwallpaper.sh
+++ .config/regolith3/i3/scripts/vwallpaper.sh
@@ -4,4 +4,6 @@
 killall -9 xwinwrap
 killall -9 mpv
 
-xwinwrap -g 1920x1080 -un -fdt -ni -b -ov -nf -- mpv -profile=wallpaper -wid WID -shuffle /home/maary/Videos/Hidamari
+# xwinwrap -g 1920x1080 -un -fdt -ni -b -ov -nf -- mpv -profile=wallpaper -wid WID -shuffle /home/maary/Videos/Hidamari
+
+xwinwrap -g 1920x1080 -un -fdt -ni -b -ov -nf -- mpv -profile=wallpaper -wid WID /home/maary/Pictures/Wallpapers/44607aab956016c08fe4449861774a9804188fa7.webm
