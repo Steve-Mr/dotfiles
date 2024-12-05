24ae343cb2ede2f4ee06a7a2255596326b514d40
Maary <24504742+Steve-Mr@users.noreply.github.com>
2024-12-05 21:17:08 +0800

    update incative windows opacity

--- .config/sway/scripts/inactive-windows-opacity.py
+++ .config/sway/scripts/inactive-windows-opacity.py
@@ -12,19 +12,14 @@ from functools import partial
 
 import i3ipc
 
-def should_ignore_window(window):
-    """
-    判断窗口是否需要跳过透明度设置：
-    - 如果窗口是浮动窗口，跳过。
-    """
-    return window.floating == "user_on" or window.floating == "auto_on"
-
-
 def should_force_full_opacity(window):
     """
     判断窗口是否需要强制设置为 100% 透明度：
+    - 如果窗口是浮动窗口，强制设置为 100%。
     - 如果窗口名称匹配规则，强制设置为 100%。
     """
+    if window.floating == "user_on" or window.floating == "auto_on":
+        return True
     if window.name and (
         "- YouTube" in window.name or
         "哔哩哔哩_bilibili" in window.name or
@@ -50,10 +45,10 @@ def on_window_focus(args, ipc, event):
     if focused.id != prev_focused.id:  # https://github.com/swaywm/sway/issues/2859
         if should_force_full_opacity(focused):
             focused.command("opacity 1.0")  # 强制设置为 100%
-        elif not should_ignore_window(focused):
+        else:
             focused.command("opacity " + args.focused)
 
-        if workspace == prev_workspace and not should_ignore_window(prev_focused):
+        if workspace == prev_workspace:
             if should_force_full_opacity(prev_focused):
                 prev_focused.command("opacity 1.0")  # 强制设置为 100%
             else:
@@ -68,7 +63,7 @@ def remove_opacity(ipc, focused_opacity):
         for window in workspace:
             if should_force_full_opacity(window):
                 window.command("opacity 1.0")  # 强制设置为 100%
-            elif not should_ignore_window(window):
+            else:
                 window.command("opacity " + focused_opacity)
     ipc.main_quit()
     sys.exit(0)
@@ -102,10 +97,6 @@ if __name__ == "__main__":
         if window.focused:
             prev_focused = window
         else:
-            # 跳过需要忽略的窗口
-            if should_ignore_window(window):
-                continue
-
             # 强制将符合规则的窗口设置为 100% 透明度
             if should_force_full_opacity(window):
                 window.command("opacity 1.0")
